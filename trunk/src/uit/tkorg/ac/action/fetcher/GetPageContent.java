869df90e13a229394a90275ae605f370cf6aa7ce
tiendo.vn@gmail.com <tiendo.vn@gmail.com@a0b170fc-c6f9-6772-6c2d-bb814d87235b>
2011-04-23 16:28:02 +0000

    firt commit

---
+++ trunk/src/uit/tkorg/ac/action/fetcher/GetPageContent.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package uit.tkorg.ac.action.fetcher;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.htmlparser.beans.StringBean;
+
+/**
+ * @author tiendv
+ *
+ */
+public class GetPageContent {
+	
+	  public static String getResults(URL source) throws IOException {
+	        
+	        InputStream in = source.openStream();
+	        StringBuffer sb = new StringBuffer();
+	        byte[] buffer = new byte[256];
+	        while(true) {
+	            int bytesRead = in.read(buffer);
+	            if(bytesRead == -1) break;
+	            for (int i=0; i<bytesRead; i++)
+	                sb.append((char)buffer[i]);
+	        }
+	        return sb.toString();
+	    }
+	  
+	    public static  String getUrlContentsAsText(String url) { 
+	        String content = ""; 
+	        StringBean stringBean = new StringBean(); 
+	        stringBean.setURL(url); 
+	        content = stringBean.getStrings(); 
+	        return content; 
+		}
+
+}
