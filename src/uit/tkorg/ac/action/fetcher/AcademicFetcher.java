afbf7c3260826f64d071c65cff7eddcb3f0df7fd
tiendo.vn@gmail.com <tiendo.vn@gmail.com@a0b170fc-c6f9-6772-6c2d-bb814d87235b>
2011-04-23 16:28:02 +0000

    firt commit

---
+++ src/uit/tkorg/ac/action/fetcher/AcademicFetcher.java
@@ -0,0 +1,25 @@
+
+package uit.tkorg.ac.action.fetcher;
+
+/**
+ * @author tiendv
+ *
+ */
+public class AcademicFetcher {
+	
+	
+	/**
+	 * String to get ID from academic when get Author do't know ID 
+	 */
+	private static String startGetUrlNOID = "http://academic.research.microsoft.com/Search?query=";
+	
+	//Keyword space = %20
+	//Example http://academic.research.microsoft.com/Search?query=tin%20huynh
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
