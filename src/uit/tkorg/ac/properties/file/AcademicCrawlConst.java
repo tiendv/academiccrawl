b9a732073d3a17adb2c9513e85ec0608bb41bdd0
minhtuyen142 <minhtuyen142@a0b170fc-c6f9-6772-6c2d-bb814d87235b>
2011-04-27 04:27:38 +0000

    - Get Conferences

--- src/uit/tkorg/ac/properties/file/AcademicCrawlConst.java
+++ src/uit/tkorg/ac/properties/file/AcademicCrawlConst.java
@@ -15,7 +15,9 @@ public class AcademicCrawlConst {
 	public static String CLASS_AUTHORS = "<div class=\'author-compact-card\'>";
 	public static String ACCADEMIC_HOMEPAGE_URL = "http://academic.research.microsoft.com";
 	public static String ACCADEMIC_COAUTHOR_URL = "http://academic.research.microsoft.com/Detail?entitytype=2&searchtype=1&id=";
+	public static String ACCADEMIC_CONFERENCE_URL = "http://academic.research.microsoft.com/Detail?entitytype=2&searchtype=3&id=";
 	public static String FIND_AUTHOR_ID = "id=^\\d*$";
 		
 	public static String COAUTHOR_PATTERN = "ctl00_MainContent_ObjectList_ctl(NUM)_authorName";
+	public static String CONFERENCE_PATTERN = "ctl00_MainContent_ObjectList_ctl(NUM)_name";
 }
